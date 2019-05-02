089dc3c47141d2833f004c80bce9b27e988f52eb
root <root@sabayon.homenetwork>
2019-05-02 11:40:48 +0200

    changement detheme plymouth

--- scripts/remaster_generic_inner_chroot_script_after.sh
+++ scripts/remaster_generic_inner_chroot_script_after.sh
@@ -434,6 +434,7 @@ prepare_system() {
 	elif [ "${de}" = "capesos" ]; then
 		prepare_xfce
 		install_packages capesos-skel
+		plymouth-set-default-theme capesos
 	elif [ "${de}" = "fluxbox" ]; then
 		prepare_fluxbox
 	elif [ "${de}" = "gnome" ]; then
