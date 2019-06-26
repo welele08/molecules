ff62d03a1bb9a994da753ca0d71650f28f5d67a1
root <root@sabayon.homenetwork>
2019-06-26 19:05:18 +0200

    changement de fond d'écran

--- scripts/generic_pre_iso_script.sh
+++ scripts/generic_pre_iso_script.sh
@@ -20,7 +20,8 @@ if [ -n "${boot_kernel}" ] && [ -f "${boot_kernel}" ]; then
 	cp "${boot_ramfs}" "${CDROOT_DIR}/boot/sabayon.igz" || exit 1
 fi
 #if [ "${remaster_type}" = "capesos" ]; then
-cp "${SABAYON_MOLECULE_HOME}/remaster/capesos/etc/xdg/menus/xfce-applications.menu" "${CHROOT_DIR}/etc/xdg/menus"|| exit 1
+#cp "${SABAYON_MOLECULE_HOME}/remaster/capesos/etc/xdg/menus/xfce-applications.menu" "${CHROOT_DIR}/etc/xdg/menus"|| exit 1
+cp -r "${SABAYON_MOLECULE_HOME}/remaster/capesos/*" "${CHROOT_DIR}"|| exit 1
 #fi
 if [ "${remaster_type}" = "KDE" ] || [ "${remaster_type}" = "GNOME" ]; then
 	isolinux_source="${SABAYON_MOLECULE_HOME}/remaster/standard_isolinux.cfg"
